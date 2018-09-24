ef77d544f0cdfa8ae988eb914d5fd9b314244247
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 17:56:33 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each.with_index { |name, index| puts "#{index+1} #{name}"
 end
 
-def summon_captain_planet(planeteer_calls)
-  planeteer_calls.map{|element| element.capitalize << "!"} 
+def summon_captain_planet(array)
+  array.map{|element| array.capitalize << "!"} 
 end
 
 def long_planeteer_calls# code an argument here
