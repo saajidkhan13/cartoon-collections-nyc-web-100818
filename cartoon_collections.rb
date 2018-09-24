8b21b73e7bad5d2f953283589df59117c4780872
Saajid Khan <saajid.khan13@stjohns.edu>
2018-09-24 17:55:44 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -2,8 +2,8 @@ def roll_call_dwarves(dwarves)
   dwarves.each.with_index { |name, index| puts "#{index+1} #{name}"
 end
 
-def summon_captain_planet# code an argument here
-  # Your code here
+def summon_captain_planet(planeteer_calls)
+  planeteer_calls.map{|element| element.capitalize << "!"} 
 end
 
 def long_planeteer_calls# code an argument here
@@ -14,4 +14,4 @@ def find_the_cheese# code an argument here
   # the array below is here to help
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
-end 
+
